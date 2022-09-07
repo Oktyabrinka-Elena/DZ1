bb52b1f8b6c947c7db530f76d4d5e8cfd5cb1d6a
ElenaZamyatina <elenzamyatina12@gmail.com>
2022-09-07 18:39:47 +0400

    ex2 done

---
+++ EX2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число A: "); //подсказка
+int NumberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: "); //подсказка
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C: "); //подсказка
+int NumberC = Convert.ToInt32(Console.ReadLine());
+
+int NumberMax = 0;
+
+if (NumberA > NumberMax) NumberMax = NumberA;
+if (NumberB > NumberMax) NumberMax = NumberB;
+if (NumberC > NumberMax) NumberMax = NumberC;
+
+Console.WriteLine("Максимальное " + NumberMax);
